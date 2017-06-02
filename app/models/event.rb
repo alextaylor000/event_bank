e7fe0b4703227023f2239075cdd54dd9e6425157
Alex Taylor <alexmctaylorpants@gmail.com>
2017-06-02 12:22:09 -0700

    Event replay! ...
    Ok, getting the event replay working was stupidly simple.
    
    Given some events...
    
    `Event.ordered.each(&:process!)`
    
    ... will create your app state!
    
    One thing I realized while writing the test for this, was that I
    shouldn't rely on factories or direct record creation to set up an
    initial state for my tests. Since *all* app state is updated via events,
    it's trivial to set up test state by just replaying whatever events
    you need to get to the initial state for the test. It's also really easy
    to reason about. :mindblown:

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,11 +2,16 @@ class Event < ActiveRecord::Base
   # Hi, I'm an abstract class!
 
   validates_presence_of :data
+  
+  scope :ordered, -> { order(:created_at) }
 
   def command
     type
   end
 
+  # TODO: we could make a before_action that converts
+  # a hash to JSON for storage, and maybe the other way
+  # for initialization
   def data
     JSON.parse(attributes.fetch('data')).symbolize_keys
   end
---
+++ spec/event_replay_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "Replaying events" do
+  let(:account_123) { Account.find_by_account_id!(123) }
+  let(:account_456) { Account.find_by_account_id!(456) }
+
+  before do
+    AccountOpen.create(data: { account_id: 123, account_owner_email: 'a@a.com' }.to_json)
+    AccountDeposit.create(data: { account_id: 123, amount_in_cents: 10_00 }.to_json)
+    AccountDeposit.create(data: { account_id: 123, amount_in_cents: 5_25 }.to_json)
+
+    AccountOpen.create(data: { account_id: 456, account_owner_email: 'b@b.com' }.to_json)
+    AccountDeposit.create(data: { account_id: 456, amount_in_cents: 250_00 }.to_json)
+
+    Event.ordered.each(&:process!)
+  end
+
+  it 'projects the right state by replaying events in the correct order' do
+    expect(account_123).to have_attributes(
+      email: 'a@a.com',
+      balance_in_cents: 15_25
+    )
+
+    expect(account_456).to have_attributes(
+      email: 'b@b.com',
+      balance_in_cents: 250_00
+    )
+  end
+end
